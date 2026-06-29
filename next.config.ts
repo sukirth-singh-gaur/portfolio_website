ed51f5f1894eeb7f104e5c5f29639c3a861fc8c7
Sukirth Singh Gaur <sukirthsinghgaur@yahoo.in>
2026-06-29 14:40:35 +0530

    changes in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  reactCompiler: true
+  //output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
