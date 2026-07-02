460f237c37195b311897a001d9a218057ec0ff50
Sukirth Singh Gaur <sukirthsinghgaur@yahoo.in>
2026-07-02 11:33:09 +0530

    next config changed for docker

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true
-  //output: 'export',
+  reactCompiler: true,
+  // output: 'export',
   // images: {
   //   unoptimized: true,
   // },
